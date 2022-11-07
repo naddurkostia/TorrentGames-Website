d64ecf56a4031245e89ec4b113e923d18ff0eca7
Kostaintyn Naddur <91757099+naddurkostia@users.noreply.github.com>
2022-11-07 16:37:45 +0200

    old vers deleted

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const shell = require('shelljs');
-
-function defaultTask(cb) {
-  shell.exec("sass --watch src/style/style.scss:src/css/style.min.css")
-	shell.exec("live-server")
-	cb();
-  }
-  
-  exports.default = defaultTask
